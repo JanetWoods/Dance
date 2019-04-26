12cc0ce04223f486117a97ff9c89051a1bf752e3
Janet Woods <janetnwoods@gmail.com>
2019-04-26 15:35:00 -0500

    removed s

--- src/modules/settings.js
+++ src/modules/settings.js
@@ -1,5 +1,4 @@
 export default {
-    remoteURL: "http://wheresthedanceapi.herokuapp.com"
-    // remoteURL: "https://wheresthedanceapi.herokuapp.com/"
+    remoteURL: "https://wheresthedanceapi.herokuapp.com/"
     // remoteURL: "http://localhost:8088"
 }
